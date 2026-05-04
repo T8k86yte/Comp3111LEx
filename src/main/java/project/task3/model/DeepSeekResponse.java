157daae3db52d202297387e3cdfa956571bfc5f9
zyangcw <zyangcw@connect.ust.hk>
2026-05-04 14:47:41 +0800

    Updated Task1 Nice to have features

--- src/main/java/project/task3/model/DeepSeekResponse.java
+++ src/main/java/project/task3/model/DeepSeekResponse.java
@@ -1,60 +1,141 @@
 package project.task3.model;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import java.util.List;
 
 public class DeepSeekResponse {
-    @Getter
-    @Setter
     private String id;
-    @Getter
-    @Setter
     private String object;
-    @Getter
-    @Setter
     private long created;
-    @Getter
-    @Setter
     private String model;
-    @Getter
-    @Setter
     private List<Choice> choices;
-    @Getter
-    @Setter
     private Usage usage;
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getObject() {
+        return object;
+    }
+
+    public void setObject(String object) {
+        this.object = object;
+    }
+
+    public long getCreated() {
+        return created;
+    }
+
+    public void setCreated(long created) {
+        this.created = created;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public List<Choice> getChoices() {
+        return choices;
+    }
+
+    public void setChoices(List<Choice> choices) {
+        this.choices = choices;
+    }
+
+    public Usage getUsage() {
+        return usage;
+    }
+
+    public void setUsage(Usage usage) {
+        this.usage = usage;
+    }
+
     public static class Choice {
-        @Getter
-        @Setter
         private int index;
-        @Getter
-        @Setter
         private Message message;
-        @Getter
-        @Setter
         private String finish_reason;
+
+        public int getIndex() {
+            return index;
+        }
+
+        public void setIndex(int index) {
+            this.index = index;
+        }
+
+        public Message getMessage() {
+            return message;
+        }
+
+        public void setMessage(Message message) {
+            this.message = message;
+        }
+
+        public String getFinish_reason() {
+            return finish_reason;
+        }
+
+        public void setFinish_reason(String finish_reason) {
+            this.finish_reason = finish_reason;
+        }
     }
 
     public static class Message {
-        @Getter
-        @Setter
         private String role;
-        @Getter
-        @Setter
         private String content;
+
+        public String getRole() {
+            return role;
+        }
+
+        public void setRole(String role) {
+            this.role = role;
+        }
+
+        public String getContent() {
+            return content;
+        }
+
+        public void setContent(String content) {
+            this.content = content;
+        }
     }
 
     public static class Usage {
-        @Getter
-        @Setter
         private int prompt_tokens;
-        @Getter
-        @Setter
         private int completion_tokens;
-        @Getter
-        @Setter
         private int total_tokens;
+
+        public int getPrompt_tokens() {
+            return prompt_tokens;
+        }
+
+        public void setPrompt_tokens(int prompt_tokens) {
+            this.prompt_tokens = prompt_tokens;
+        }
+
+        public int getCompletion_tokens() {
+            return completion_tokens;
+        }
+
+        public void setCompletion_tokens(int completion_tokens) {
+            this.completion_tokens = completion_tokens;
+        }
+
+        public int getTotal_tokens() {
+            return total_tokens;
+        }
+
+        public void setTotal_tokens(int total_tokens) {
+            this.total_tokens = total_tokens;
+        }
     }
 }
